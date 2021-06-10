de483059ba640be8b9eaa3aa003f4b4f418962ac
Abdelrhman Kadry <49434079+abdelrhmankadry@users.noreply.github.com>
2021-06-10 21:26:24 +0200

    Add files via upload

--- mancala.py
+++ mancala.py
@@ -1,3 +1,9 @@
+import time
+def get_possible_actoins(s):
+    if s[1] == 1:
+        return [i for i, value in enumerate(s[0][0:6]) if not value ==0]
+    else:
+        return  [i+7 for i, value in enumerate(s[0][7:13]) if not value ==0]
 
 def get_result_state_without_steeling(s, a):
     #TODO: raise exception if unallowed action is passed
@@ -51,3 +57,64 @@ def get_result_state_with_steeling(s, a):
     else: 
         s_new[1] = int(not bool(s[1]))
     return tuple(s_new)
+
+def is_end_game(s):
+    if all([i ==0 for i in s[0][:6]]) or all([i ==0 for i in s[0][7:13]]):
+        #print(s)
+        return True
+    else: 
+        return False
+
+def evaluate_state_score(s):
+    if is_end_game(s):
+        if s[0][6] > s[0][13]:
+            return float('inf')
+        elif s[0][6] < s[0][13]:
+            return float('-inf')
+        else:
+            return 0
+    return   (s[0][6] - s[0][13]) 
+    
+def max_value(s, depth, alpha= float('-inf'), beta = float('inf'), stealing=True):
+
+    if is_end_game(s) or depth == 0:
+        return evaluate_state_score(s), -1
+    v =  float('-inf')
+    action = -1
+    for a in get_possible_actoins(s):
+        v_prev = v
+        if stealing:
+            v = max(v, min_value(get_result_state_with_steeling(s,a), depth-1, alpha, beta)[0])
+        else:
+            v = max(v, min_value(get_result_state_without_steeling(s,a), depth-1, alpha, beta)[0])
+        if not v == v_prev:
+            action = a
+        if v > alpha:
+            alpha = v
+        if alpha > beta:
+            break
+    return v, action
+
+def min_value(s, depth, alpha= float('-inf'), beta = float('inf'), stealing = True):
+    if is_end_game(s) or depth == 0:
+        return evaluate_state_score(s), -1
+    v =  float('inf')
+    action =-1
+
+    for a in get_possible_actoins(s):
+        v_prev = v
+        if stealing:
+            v = min(v, max_value(get_result_state_with_steeling(s,a), depth-1, alpha, beta)[0])
+        else:
+            v = min(v, max_value(get_result_state_without_steeling(s,a), depth-1, alpha, beta)[0])
+        if not v == v_prev:
+            action = a
+        if v < beta:
+            beta = v
+        if alpha > beta:
+            break
+    return v, action
+# start_time = time.time()
+# print(min_value(([4,4,4,4,4,4,0,4,4,4,4,4,4,0],0), 10))
+# end_time = time.time()
+# print("Duration:" + str(end_time - start_time))
