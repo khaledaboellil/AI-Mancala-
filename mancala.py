fafb07a3f8fa7e062f10492c2095fdd2cf29f36a
Abdelrahman Mohamed Ayman <49798366+AbdoAyman753@users.noreply.github.com>
2021-06-10 23:27:48 +0200

    Update mancala.py

--- mancala.py
+++ mancala.py
@@ -1,120 +1,272 @@
-import time
-def get_possible_actoins(s):
-    if s[1] == 1:
-        return [i for i, value in enumerate(s[0][0:6]) if not value ==0]
-    else:
-        return  [i+7 for i, value in enumerate(s[0][7:13]) if not value ==0]
+from random import randint
+from datetime import datetime
+from mancala import max_value, min_value
+board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+thisdict = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 7,
+    "H": 8,
+    "I": 9,
+    "J": 10,
+    "K": 11,
+    "L": 12,
+}
+loadList = []
 
-def get_result_state_without_steeling(s, a):
-    #TODO: raise exception if unallowed action is passed
-    board, player = s[:]
-    s_new = board[:], player
-    s_new = list(s_new)
-    num_of_stones = s[0][a]
-    index = 1
-    s_new[0][a] = 0
-    while num_of_stones > 0:
-        if (s[1] == 0 and (a+index)% 14 == 6) or (s[1] == 1 and (a+index)% 14 == 13):
-            index+=1
-        s_new[0][(a+index)% 14] +=1
-        index = (index +1) % 14
-        num_of_stones-=1
-    index-=1
-    if (a+index)% 14 == 6:
-        s_new[1] = 1
-    elif (a+index)% 14 == 13:
-        s_new[1] = 0
-    else: 
-        s_new[1] = int(not bool(s[1]))
-    return tuple(s_new)
+def convert(str):
+    return thisdict[str.upper()]
 
-def get_result_state_with_steeling(s, a):
-    #TODO: raise exception if unallowed action is passed
-    board, player = s[:]
-    s_new = board[:], player
-    s_new = list(s_new)
-    num_of_stones = s[0][a]
-    index = 1
-    s_new[0][a] = 0
-    while num_of_stones > 0:
-        if (s[1] == 0 and (a+index)% 14 == 6) or (s[1] == 1 and (a+index)% 14 == 13):
-            index+=1
-        s_new[0][(a+index)% 14] +=1
-        index = (index +1) % 14
-        num_of_stones-=1
-    index-=1
-    last_index = (a+index)% 14
-    if s_new[0][last_index] == 1 and  (not last_index == 6) and (not last_index == 13)\
-         and ((last_index < 6 and s[1] or last_index > 6 and not s[1])):
 
-        s_new[0][last_index] = 0
-        s_new[0][6*s[1]+13*(1-s[1])]+=s_new[0][12-last_index]+1
-        s_new[0][12-last_index] = 0
-    if last_index == 6:
-        s_new[1] = 1
-    elif last_index == 13:
-        s_new[1] = 0
-    else: 
-        s_new[1] = int(not bool(s[1]))
-    return tuple(s_new)
+def get_key(val):
+    for key, value in thisdict.items():
+        if val == value:
+            return key
 
-def is_end_game(s):
-    if all([i ==0 for i in s[0][:6]]) or all([i ==0 for i in s[0][7:13]]):
-        #print(s)
+
+def listToString(s):
+    # initialize an empty string
+    return ' '.join([str(elem) for elem in s])
+
+
+def save(board, playerOne, withBot, mode, stealing):
+    f = open("data.txt", "a")
+    st = listToString(
+        [str(datetime.now()), ",", listToString(board), ",", str(playerOne), ",", str(withBot), ",", str(mode), ",", str(stealing), "\n"])
+    f.write(st)
+    f.close()
+
+
+def load():
+    f = open("data.txt", "r")
+    string = ""
+    string = f.read()
+    loadList = str(string).split("\n")
+    for i in range(len(loadList) - 1):
+       print(i + 1, "    ", str(loadList[i]).replace(",", "     "))
+    loadGame = int(input("Pick a game to load: "))
+    string = loadList[loadGame - 1]
+    loaded = str(string).split(",")
+    f.close()
+    return loaded
+
+
+def Game_design(board):
+    print("    ,|L:{}|,|K:{}|,|J:{}|,|I:{}|,|H:{}|,|G:{}|,".format(board[12], board[11], board[10], board[9], board[8],
+                                                                   board[7]))
+    print("|{}|                                          |{}|".format(board[13], board[6]))
+    print("    ,|A:{}|,|B:{}|,|C:{}|,|D:{}|,|E:{}|,|F:{}|,".format(board[0], board[1], board[2], board[3], board[4],
+                                                                   board[5]))
+
+
+def moving(board, num, stealing):
+    flag = 0
+    sum = board[num]
+    board[num] = 0
+    index = num + 1
+    for i in range(sum):
+
+        if index == 6:
+            if num > 5:
+                index += 1
+                board[index % 14] += 1
+            else:
+                board[index % 14] += 1
+        elif index == 13:
+            if num <= 5:
+                index += 1
+                board[index % 14] += 1
+            else:
+                board[index % 14] += 1
+        else:
+            board[index % 14] += 1
+
+        index += 1
+    vr = index - 1 % 14
+    if (stealing):
+        if num <= 5 and vr <= 5:
+            if board[vr] == 1:
+                if board[12 - vr] != 0:
+                    board[6] += (board[12 - vr] + 1)
+                    board[12 - vr], board[vr] = 0, 0
+        elif num > 6 and 6 < vr < 13:
+            if board[vr] == 1:
+                if board[12 - vr] != 0:
+                    board[13] += (board[12 - vr] + 1)
+                    board[vr], board[12 - vr] = 0, 0
+
+    if vr == 6 or vr == 13:
+        flag = 1
+
+    return flag
+
+
+
+def is_end(board):  # check if game is end
+    if sum(board[0:6]) == 0 or sum(board[7:13]) == 0:
+        board[13] += sum(board[7:13])
+        board[6] += sum(board[0:6])
+        for i in range(14):
+            if i != 13 and i != 6:
+                board[i] = 0
         return True
-    else: 
-        return False
+    return False
 
-def evaluate_state_score(s):
-    if is_end_game(s):
-        if s[0][6] > s[0][13]:
-            return float('inf')
-        elif s[0][6] < s[0][13]:
-            return float('-inf')
-        else:
-            return 0
-    return   (s[0][6] - s[0][13]) 
-    
-def max_value(s, depth, alpha= float('-inf'), beta = float('inf'), stealing=True):
 
-    if is_end_game(s) or depth == 0:
-        return evaluate_state_score(s), -1
-    v =  float('-inf')
-    action = -1
-    for a in get_possible_actoins(s):
-        v_prev = v
-        if stealing:
-            v = max(v, min_value(get_result_state_with_steeling(s,a), depth-1, alpha, beta)[0])
+def end_of_game(board):  # if game is end so print scores
+    flag = 0
+    if is_end(board):
+        Game_design(board)
+        flag = 1
+        print("Game over")
+        if board[6] > board[13]:
+            print("player one win ")
+            print("score", board[6], ":", board[13])
+        elif board[6] == board[13]:
+            print("Draw")
+            print("score", board[6], ":", board[13])
+
         else:
-            v = max(v, min_value(get_result_state_without_steeling(s,a), depth-1, alpha, beta)[0])
-        if not v == v_prev:
-            action = a
-        if v > alpha:
-            alpha = v
-        if alpha > beta:
+            print("player two win ")
+            print("score", board[13], ":", board[6])
+        for i in range(14):
+            if i != 13 and i != 6:
+                board[i] = 0
+    return flag
+
+
+playerone = True
+withBot = False
+bot = False
+stealing = 0
+depth = 7
+value = 0
+loadGame = int(input("Enter 0 for New Game, 1 to Load a saved game: "))
+if loadGame == 1:
+    loadedGame = load()
+    temp = loadedGame[1].split(" ")
+    for i in range(len(temp)):
+        board [i] = int(temp[i])
+    if loadedGame[2] != "True" and loadedGame[3] == "True":
+        playerone = False
+        withBot = True
+        bot = True
+        print("=================== Bot ===================")
+    elif loadedGame[2] != "True" and loadedGame[3] == "False":
+        playerone = False
+        print("=============== Player Two ===============")
+    else:
+        print("=============== Player One ===============")
+    Game_design(board)
+    print("==========================================")
+    depth = int(loadedGame[3])
+    stealing = int(loadedGame[4])
+else:
+    stealing = int(input("Enter 1 for stealing and 0 for not stealing: "))
+    withBot = bool(int(input("Enter 0 to play against player or 1 to play against bot: ")))
+    if withBot:
+        mode = int(input("Enter 1 for easy ,2 for medium and 3 for hard "))
+        if mode == 1:
+            depth = 4
+        elif mode == 2:
+            depth = 7
+        else:
+            depth = 10
+        value = int(input("Enter 0 to start first or 1 to let bot start: "))  # if value = 0 player start else bot start
+    if value == 0:
+        print("=============== Player One ===============")
+    elif withBot and value == 1:
+        playerone = False
+        bot = True
+        print("=================== Bot ===================")
+    # else:
+    #     playerone = False
+    #     bot = False
+    #     print("=============== Player Two ===============")
+    Game_design(board)
+    print("==========================================")
+
+
+playing = True
+while playing:
+
+    playagain = 0
+    while playerone:
+        num = input("player one Enter the number: ")
+        if num == "save":
+            save(board, playerone, depth, stealing)
+            num = input("player one Enter the number: ")
+        num = convert(num)
+        if 0 <= num <= 5 and board[num] != 0:
+            playagain = moving(board, num, stealing)
+            print("Move ==> {}".format(get_key(num)))
+            if withBot:
+                print("================ Bot =================")
+            else :
+                print("============ Player Two ==============")
+            Game_design(board)
+            print("==========================================")
+            if playagain != 1:
+                playerone = False
+        else:
+            print("invalid play")
+            print("\n")
+        if withBot:
+            bot = True
+
+    playagain = 0
+    if end_of_game(board):
+        playing = False
+        break
+
+    while not playerone and not withBot:
+        num = input("player Two Enter the number: ")
+        if num == "save":
+            save(board, playerone, depth, stealing)
+            num = input("player Two Enter the number: ")
+        num = convert(num)
+        if 7 <= num <= 13 and board[num] != 0:
+            playagain = moving(board, num, stealing)
+            print("Move ==> {}".format(get_key(num)))
+            if withBot:
+                print("================ Bot =================")
+            else:
+                print("============ Player One ==============")
+            Game_design(board)
+            print("==========================================")
+            if playagain != 1:
+                playerone = True
+        else:
+            print("invalid play")
+            print("\n")
+
+    if end_of_game(board):
+        playing = False
+        break
+
+    while bot and withBot:
+        _, num = min_value((board, 0), depth, stealing=stealing)
+        if 7 <= num <= 12 and board[num] != 0:
+            playagain = moving(board, num, stealing)
+            print("Move ==> {}".format(get_key(num)))
+            print("================= Bot ====================")
+            Game_design(board)
+            print("==========================================")
+            if playagain != 1:
+                bot = False
+        elif num == -1:
+            end_of_game(board)
+            playing = False
             break
-    return v, action
-
-def min_value(s, depth, alpha= float('-inf'), beta = float('inf'), stealing = True):
-    if is_end_game(s) or depth == 0:
-        return evaluate_state_score(s), -1
-    v =  float('inf')
-    action =-1
-
-    for a in get_possible_actoins(s):
-        v_prev = v
-        if stealing:
-            v = min(v, max_value(get_result_state_with_steeling(s,a), depth-1, alpha, beta)[0])
         else:
-            v = min(v, max_value(get_result_state_without_steeling(s,a), depth-1, alpha, beta)[0])
-        if not v == v_prev:
-            action = a
-        if v < beta:
-            beta = v
-        if alpha > beta:
-            break
-    return v, action
-# start_time = time.time()
-# print(min_value(([4,4,4,4,4,4,0,4,4,4,4,4,4,0],0), 10))
-# end_time = time.time()
-# print("Duration:" + str(end_time - start_time))
+            print("invalid play")
+            print("\n")
+        playerone = True
+
+    if end_of_game(board):
+        playing = False
+        break
